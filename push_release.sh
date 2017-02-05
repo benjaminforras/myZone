9038e0c95d8d2157a16b611c446fe62fb5630dec
Benjamin Forrás <rsnconfigs@gmail.com>
2017-02-05 10:13:28 +0100

    Update push_release.sh

--- push_release.sh
+++ push_release.sh
@@ -9,16 +9,17 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
     # Is this not a build which was triggered by setting a new tag?
     if [ -z "$TRAVIS_TAG" ]; then
       echo -e "Starting to push release.\n"
-
-      git config --global user.email "travis@travis-ci.org"
-      git config --global user.name "Travis"
-
-      # Add tag and push to master.
-      git tag -a ${TRAVIS_BRANCH}.${TRAVIS_BUILD_NUMBER} -m "Travis build ${TRAVIS_BRANCH}.${TRAVIS_BUILD_NUMBER} pushed a tag."
-      git push origin --tags
+      
+      git config --global user.email "builds@travis-ci.com"
+      git config --global user.name "Travis CI"
+      
+      export GIT_TAG=" $TRAVIS_BRANCH.$TRAVIS_BUILD_NUMBER-SNAPSHOT" 
+      git tag $GIT_TAG -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
+      git push -q https://$GITHUBKEY@github.com/TryHardDood/myZone $GIT_TAG
+      
       git fetch origin
-
+      
       echo -e "Done pushing the release.\n"
-  fi
+    fi
   fi
 fi
